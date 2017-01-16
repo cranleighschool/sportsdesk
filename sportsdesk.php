1902488db1232372f76fae5f6efa5aed76052b15
Fred Bradley <hello@fredbradley.co.uk>
2017-01-16 11:54:08 +0000

    Remove cranfont from plugin - deciding to do this from within the theme instead

--- sportsdesk.php
+++ sportsdesk.php
@@ -34,20 +34,6 @@ class cran_SportsDesk {
 		add_filter('the_content',array(&$this,'awayfixture_content_filter'));
 
 		add_action('sportsdesk_daily', array($this, 'run_sync'));
-		add_action('admin_head', array($this, 'custom_icon'));
-		add_action('admin_enqueue_scripts', array($this, 'admin_style'));
-
-	}
-	function admin_style() {
-		wp_enqueue_style( 'cranfont', '//cdn.cranleigh.org/cranfont/style.css' );
-	}
-	function custom_icon() {
-		echo '<style type="text/css" media="screen">
-			#adminmenu .menu-icon-match div.wp-menu-image:before {
-				font-family: cranfont !important;
-				content: "\e915"
-			}
-			</style>';
 	}
 
 	function plugin_activation() {
