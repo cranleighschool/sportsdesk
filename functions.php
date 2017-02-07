df4ed7c7bc829a3e8b05cbfb10a1f83cc685b851
Fred Bradley <hello@fredbradley.co.uk>
2017-02-07 16:37:10 +0000

    Added function `cranleigh_sportsdesk_check_week_term_exists()` which is used in `taxonomoy-week.php` in the theme.

--- functions.php
+++ functions.php
@@ -79,5 +79,46 @@ function what_week(){
             return 'Summer '.$year;
         }
    }
-?>
+
+	/**
+	 * cranleigh_sportsdesk_check_week_term_exists function.
+	 *
+	 * @author Fred Bradley <frb@cranleigh.org>
+	 * @param int $week_number
+	 * @param string $term_name
+	 * @param int $year
+	 * @param string $which
+	 * @param int $attempt (default: 1)
+	 * @return instance of get_term_by() if successful, or WP_Error if failure.
+	 */
+	function cranleigh_sportsdesk_check_week_term_exists(int $week_number, string $term_name, int $year, string $which, int $attempt=1) {
+		/* Formulate the Slug - because we wrote the sportsdesk that creates these slugs, we now how to formulate the slug string... */
+		$slug = "week-".$week_number."-".$term_name."-".$year;
+
+		/* Check to see if the term that we're parsing exists... */
+		if (term_exists( $slug, 'week')) {
+			// If the term exists let's get it and return it as an object...
+			return get_term_by("slug", $slug, 'week');
+		} else {
+			// If the term doesn't exist we want to try again..
+
+			//But we only want to try again one. If we've landed here twice (See $attempt param) then let's duck outta here
+			if ($attempt > 1) {
+				return new WP_Error(404, __("Tried twice to find the taxonomy for the ".$which." week, but alas, I failed!", "cranleigh"));
+			}
+
+			/*
+			 * So we're still alive, so let's continue to try and find the next week...
+			 * We use the $which variable to work out whether we're looking a week behind or a week ahead...
+			 * and literally run the function again!
+			 */
+			if ($which=="previous") {
+				return cranleigh_sportsdesk_check_week_term_exists(($week_number-1), $term_name, $year, $which, 2);
+			} elseif ($which=="next") {
+				return cranleigh_sportsdesk_check_week_term_exists(($week_number+1), $term_name, $year, $which, 2);
+			}
+		}
+		// If we got this far then something went wrong, but let's give a graceful error anyway!
+		return new WP_Error(500, __( "I shouldn't have got this far... something's wrong.", "cranleigh" ) );
+	}
 
