fa87d9dd3464d34087db24d20312e549eda33cf2
Fred Bradley <hello@fredbradley.co.uk>
2017-02-08 16:18:27 +0000

    Taken debug out

--- widgets/team.php
+++ widgets/team.php
@@ -52,8 +52,7 @@ class SportsDesk_Team_Widget extends WP_Widget {
     		var dropdown_team = document.getElementById("teamcat");
   		function onTeamCatChange() {
                 	if ( dropdown_team.options[dropdown_team.selectedIndex].value != -1 ) {
-	                	alert(dropdown_team.options[dropdown_team.selectedIndex].value);
-                        	location.href = "<?php echo get_option('cran_sportsdesk_slug');?>team/"+dropdown_team.options[dropdown_team.selectedIndex].value+"/";
+						location.href = "<?php echo get_option('cran_sportsdesk_slug');?>team/"+dropdown_team.options[dropdown_team.selectedIndex].value+"/";
                 	}
     		}
     		dropdown_team.onchange = onTeamCatChange;
