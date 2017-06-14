cd34cecb508512871bddbc3e74e36aaf9a088c25
Fred Bradley <hello@fredbradley.co.uk>
2017-06-14 10:36:05 +0100

    Fix url in widgets to use the new `sportsdesk_slug` option

--- widgets/archive.php
+++ widgets/archive.php
@@ -36,18 +36,18 @@ class SportsDesk_Archive_Widget extends WP_Widget {
 		add_filter('get_terms_orderby', array($this,"get_terms_orderby_natural_slug"),10,2);
 		$select = wp_dropdown_categories( 'taxonomy=week&hierarchical=1&orderby=name&id=archivecat&echo=0' );
 		remove_filter('get_terms_orderby', array($this,"get_terms_orderby_natural_slug"));
-		
+
                 $select = preg_replace_callback("#<option[^>]*>[^<]*</option>#", array($this,"replace_catid_for_slug"), $select);
 
                 echo $select;
-	
-		#print_r(get_categories('taxonomy=week')); 
+
+		#print_r(get_categories('taxonomy=week'));
 		?>
 	     <script type="text/javascript"><!--
                 var dropdown = document.getElementById("archivecat");
                 function onCatChange() {
                         if ( dropdown.options[dropdown.selectedIndex].value != -1 ) {
-                                location.href = "<?php echo get_option('home');?>/week/"+dropdown.options[dropdown.selectedIndex].value+"/";
+                                location.href = "<?php echo get_option('home');?><?php echo get_option('cran_sportsdesk_slug'); ?>week/"+dropdown.options[dropdown.selectedIndex].value+"/";
                         }
                 }
                 dropdown.onchange = onCatChange;
@@ -87,21 +87,21 @@ class SportsDesk_Archive_Widget extends WP_Widget {
 		else {
 			$title = 'Results Archive';
 		}
-		
+
 		?>
 		<p>
-		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php print "Widget Title"; ?></label> 
+		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php print "Widget Title"; ?></label>
 		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
 		</p>
 
-		<?php 
+		<?php
 	}
-	
+
 	function get_terms_orderby_natural_slug($orderby, $args){
     		$orderby = "SUBSTR({$orderby} FROM 1 FOR 5), CAST(SUBSTR({$orderby} FROM 6) AS UNSIGNED), SUBSTR({$orderby} FROM 6)";
     		return $orderby;
 	}
-	
+
         // From http://wordpress.org/support/topic/i-need-to-get-the-category-slug-from-the-category-id
         function replace_catid_for_slug($option){
                 $categories = get_categories("taxonomy=week&hide_empty=0");
@@ -121,3 +121,4 @@ class SportsDesk_Archive_Widget extends WP_Widget {
 
 
 } // End of widget class
+
